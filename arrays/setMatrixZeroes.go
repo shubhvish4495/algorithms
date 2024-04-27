5b3e9b1782695b71e2125923dc87ea92c6348ea6
Shubham Saurav <shubhamsaurav001@gmail.com>
2024-04-27 13:42:50 +0530

    solution

--- arrays/setMatrixZeroes.go
+++ arrays/setMatrixZeroes.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
+// leetcode problem link -> https://leetcode.com/problems/set-matrix-zeroes/description/
+
 func main() {
-	// test
+	a := [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}
+	// a := [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
+
+	// zeroColRowVal :=
+	zeroRowVal := make(map[int]struct{})
+	zeroColVal := make(map[int]struct{})
+
+	for i := range a {
+		for j := range a[i] {
+			if a[i][j] == 0 {
+				zeroRowVal[i] = struct{}{}
+				zeroColVal[j] = struct{}{}
+			}
+		}
+	}
+
+	for i := range zeroRowVal {
+		for j := range a[i] {
+			a[i][j] = 0
+		}
+	}
+
+	for j := range zeroColVal {
+		for i := range len(a) {
+			a[i][j] = 0
+		}
+	}
+
+	fmt.Print(a)
 }
